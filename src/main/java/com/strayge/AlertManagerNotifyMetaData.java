2fd6055e210b75505c8a9d631c544d58581bc9b1
strayge <strayge@gmail.com>
2022-01-26 05:01:58 +0300

    initial plugin boilerplate

---
+++ src/main/java/com/strayge/AlertManagerNotifyMetaData.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package com.strayge;
+
+import org.graylog2.plugin.PluginMetaData;
+import org.graylog2.plugin.ServerStatus;
+import org.graylog2.plugin.Version;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Implement the PluginMetaData interface here.
+ */
+public class AlertManagerNotifyMetaData implements PluginMetaData {
+    private static final String PLUGIN_PROPERTIES = "com.strayge.graylog-plugin-alertmanagernotify/graylog-plugin.properties";
+
+    @Override
+    public String getUniqueId() {
+        return "com.strayge.AlertManagerNotifyPlugin";
+    }
+
+    @Override
+    public String getName() {
+        return "AlertManagerNotify";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "strayge";
+    }
+
+    @Override
+    public URI getURL() {
+        return URI.create("https://github.com/strayge/graylog-plugin-alertmanagernotify");
+    }
+
+    @Override
+    public Version getVersion() {
+        return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "version", Version.from(0, 0, 0, "unknown"));
+    }
+
+    @Override
+    public String getDescription() {
+        // TODO Insert correct plugin description
+        return "Description of AlertManagerNotify plugin";
+    }
+
+    @Override
+    public Version getRequiredVersion() {
+        return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "graylog.version", Version.from(0, 0, 0, "unknown"));
+    }
+
+    @Override
+    public Set<ServerStatus.Capability> getRequiredCapabilities() {
+        return Collections.emptySet();
+    }
+}
---
+++ src/main/java/com/strayge/AlertManagerNotifyModule.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 Graylog, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package com.strayge;
+
+import org.graylog2.plugin.PluginConfigBean;
+import org.graylog2.plugin.PluginModule;
+
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Extend the PluginModule abstract class here to add you plugin to the system.
+ */
+public class AlertManagerNotifyModule extends PluginModule {
+    /**
+     * Returns all configuration beans required by this plugin.
+     *
+     * Implementing this method is optional. The default method returns an empty {@link Set}.
+     */
+    @Override
+    public Set<? extends PluginConfigBean> getConfigBeans() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    protected void configure() {
+        /*
+         * Register your plugin types here.
+         *
+         * Examples:
+         *
+         * addMessageInput(Class<? extends MessageInput>);
+         * addMessageFilter(Class<? extends MessageFilter>);
+         * addMessageOutput(Class<? extends MessageOutput>);
+         * addPeriodical(Class<? extends Periodical>);
+         * addAlarmCallback(Class<? extends AlarmCallback>);
+         * addInitializer(Class<? extends Service>);
+         * addRestResource(Class<? extends PluginRestResource>);
+         *
+         *
+         * Add all configuration beans returned by getConfigBeans():
+         *
+         * addConfigBeans();
+         */
+    }
+}
